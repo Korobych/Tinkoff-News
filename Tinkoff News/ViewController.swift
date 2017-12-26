61dfb79a9cb3371b507044b952d86faf2c871761
Sergey Korobin <cerkill@mail.ru>
2017-12-26 12:24:12 +0300

    Initial Commit

---
+++ Tinkoff News/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tinkoff News
+//
+//  Created by Sergey Korobin on 26.12.17.
+//  Copyright © 2017 SergeyKorobin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
